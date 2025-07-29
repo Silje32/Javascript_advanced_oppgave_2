acae6203df39181bc036b101b87836efe8602e1c
Silje32 <msh070425@gmail.com>
2025-07-29 15:02:44 +0200

    Made my addData function and added comments

---
+++ app.js
@@ -0,0 +1,14 @@
+/* A system that logs watched movies and series and rates them */
+
+// Add Data to localStorage
+function addData() {}
+
+// Show Data
+
+// Update Data and add as favourite or recommended
+
+// Remove Data
+
+// Filter Data
+
+// Sort Data alphanumeric or by date
